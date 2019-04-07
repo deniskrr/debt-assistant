2c591736feec75ec9fc2e453f315923b617e1699
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-07 18:22:38 +0300

    Add tips fragment

---
+++ app/src/main/java/com/worldconnect/debtassistant/ui/tips/TipsViewModel.kt
@@ -0,0 +1,13 @@
+package com.worldconnect.debtassistant.ui.tips
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class TipsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tips Fragment"
+    }
+    val text: LiveData<String> = _text
+}
