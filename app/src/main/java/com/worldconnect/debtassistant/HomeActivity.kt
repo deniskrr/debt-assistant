04277aa30767201bc2cd9abed5b00529ccdf20b7
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-07 18:47:43 +0300

    Add Home Activity + navigation connections

---
+++ app/src/main/java/com/worldconnect/debtassistant/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.worldconnect.debtassistant
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        nav_view.setupWithNavController(navController)
+
+    }
+}
