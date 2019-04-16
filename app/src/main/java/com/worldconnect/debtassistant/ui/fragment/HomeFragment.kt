9d5054eccc5bfc484f278439064d15e49eb80d83
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-16 16:31:20 +0300

    Inflate the right layout in HomeFragment

--- app/src/main/java/com/worldconnect/debtassistant/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/worldconnect/debtassistant/ui/fragment/HomeFragment.kt
@@ -22,11 +22,8 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_overview, container, false)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_overview)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
