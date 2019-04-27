85879fe1813ca84df24cf8262d0b6ddce4c6ecda
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-27 14:52:33 +0300

    Remove TipsViewModel
    It is not necessary

--- app/src/main/java/com/worldconnect/debtassistant/ui/fragment/TipsFragment.kt
+++ app/src/main/java/com/worldconnect/debtassistant/ui/fragment/TipsFragment.kt
@@ -4,29 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.worldconnect.debtassistant.R
-import com.worldconnect.debtassistant.viewmodel.TipsViewModel
 
 class TipsFragment : Fragment() {
-
-    private lateinit var tipsViewModel: TipsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        tipsViewModel =
-            ViewModelProviders.of(this).get(TipsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tips, container, false)
-        val textView: TextView = root.findViewById(R.id.text_tips)
-        tipsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
