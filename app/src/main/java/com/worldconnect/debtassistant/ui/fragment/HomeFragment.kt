c8677d090b66093a79779779d8031786657826c4
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-28 15:35:23 +0300

    Add begin test button.
    It disables Advice menu, enables (because it is disabled after a test ended) and navigates to Test menu

--- app/src/main/java/com/worldconnect/debtassistant/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/worldconnect/debtassistant/ui/fragment/HomeFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.worldconnect.debtassistant.R
 import com.worldconnect.debtassistant.ui.activities.StartActivity
+import com.worldconnect.debtassistant.viewmodel.TestViewModel
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    private lateinit var testViewModel: TestViewModel
 
     var name: String? = ""
 
@@ -26,5 +31,16 @@ class HomeFragment : Fragment() {
 
         name = activity?.intent?.extras?.get(StartActivity.NAME).toString()
         text_name.text = name
+
+        button_begin_test.setOnClickListener {
+
+            testViewModel = ViewModelProviders.of(activity!!).get(TestViewModel::class.java)
+            testViewModel.reset()
+
+            activity!!.nav_view.menu.findItem(R.id.navigation_advice).isEnabled = false
+            activity!!.nav_view.menu.findItem(R.id.navigation_test).isEnabled = true
+
+            activity!!.nav_view.selectedItemId = R.id.navigation_test
+        }
     }
 }
